210e15ed90b694de3cb643e5eaad481a52051371
pramodpachpule <101208691+pramodpachpule@users.noreply.github.com>
2022-06-06 12:20:25 +0530

    Update Multiple_Disease_Prediction.py

--- Multiple_Disease_Prediction.py
+++ Multiple_Disease_Prediction.py
@@ -1,56 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  8 21:01:15 2022
+
+@author: siddhardhan
+"""
+
 import pickle
-from pyparsing import col
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
-heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
-parkinsons_model = pickle.load(open('parkinsons_model.sav','rb'))
+
+# loading the saved models
+
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
-# sidebar 
+
+# sidebar for navigation
 with st.sidebar:
+    
     selected = option_menu('Multiple Disease Prediction System',
-    ['Diabetes Prediction',
-     'Heart Disease Prediction',
-     'Parkinsons Prediction'],
-     icons= ['activity','heart','person'],
-    default_index= 0)
-
-
-if (selected =='Diabetes Prediction'):
-    st.title('Diabetes Prediction Using ML')
-
+                          
+                          ['Diabetes Prediction',
+                           'Heart Disease Prediction',
+                           'Parkinsons Prediction'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+    
+    
+# Diabetes Prediction Page
+if (selected == 'Diabetes Prediction'):
+    
+    # page title
+    st.title('Diabetes Prediction using ML')
+    
+    
+    # getting the input data from the user
     col1, col2, col3 = st.columns(3)
-    with col1 :
+    
+    with col1:
         Pregnancies = st.text_input('Number of Pregnancies')
-    with col2 :
-        Glucose = st.text_input('Glucose Level')
-    with col3:
-        BloodPressure = st.text_input('Blood Pressure')
-    with col1 :
-        SkinThickness = st.text_input('Skin Thickness')
-    with col2 :
-        Insulin = st.text_input('Insulin Level')
-    with col3:
-        BMI = st.text_input('Body Mass Index')
-    with col1 :
-        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function')
+        
     with col2:
-        Age = st.text_input('Age')
-
-    dia_diagnosis = ''
+        Glucose = st.text_input('Glucose Level')
+    
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+    
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+    
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+    
+    with col3:
+        BMI = st.text_input('BMI value')
+    
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    
+    with col2:
+        Age = st.text_input('Age of the Person')
+    
+    
+    # code for Prediction
+    diab_diagnosis = ''
+    
+    # creating a button for Prediction
+    
     if st.button('Diabetes Test Result'):
-        dia_prediction  = diabetes_model.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction, Age]])
-        if dia_prediction[0] == 1 :
-            dia_diagnosis = 'The Person is Diabetic'
+        diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        
+        if (diab_prediction[0] == 1):
+          diab_diagnosis = 'The person is diabetic'
         else:
-            dia_diagnosis = 'The Person is not Diabetic' 
-    st.success(dia_diagnosis)
+          diab_diagnosis = 'The person is not diabetic'
+        
+    st.success(diab_diagnosis)
 
-if (selected =='Heart Disease Prediction'):
-    st.title('Heart Disease Prediction Using ML')
+
+
+
+# Heart Disease Prediction Page
+if (selected == 'Heart Disease Prediction'):
+    
+    # page title
+    st.title('Heart Disease Prediction using ML')
+    
     col1, col2, col3 = st.columns(3)
+    
     with col1:
         age = st.text_input('Age')
         
@@ -89,8 +131,15 @@ if (selected =='Heart Disease Prediction'):
         
     with col1:
         thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
-
+        
+        
+     
+     
+    # code for Prediction
     heart_diagnosis = ''
+    
+    # creating a button for Prediction
+    
     if st.button('Heart Disease Test Result'):
         heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,oldpeak,slope,ca,thal]])                          
         
@@ -101,7 +150,8 @@ if (selected =='Heart Disease Prediction'):
         
     st.success(heart_diagnosis)
         
-
+    
+    
 
 # Parkinson's Prediction Page
 if (selected == "Parkinsons Prediction"):
@@ -192,3 +242,18 @@ if (selected == "Parkinsons Prediction"):
           parkinsons_diagnosis = "The person does not have Parkinson's disease"
         
     st.success(parkinsons_diagnosis)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
